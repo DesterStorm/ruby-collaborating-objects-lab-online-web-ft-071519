0a512bfdc0dcdf6fe63a24b3a8ab9be42e7b5a8d
Daniel Siekawitch <daniel.siekawitch@gmail.com>
2019-08-10 17:54:21 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,7 +2,7 @@ class Artist
 
   attr_accessor :name, :songs
 
-  @@all = [] # array of artists instances
+  @@all = [] # array of artist instances
 
   def initialize(name) # artist_name
     @name = name
